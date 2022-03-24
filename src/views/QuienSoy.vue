1af944b7bcdcee32d57d2b840508f7a1746fd6f5
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2022-03-23 21:59:38 -0600

    Estilo a la carta donde me presento

--- src/views/QuienSoy.vue
+++ src/views/QuienSoy.vue
@@ -6,7 +6,7 @@
             <div class="card-body">
                 <p class="card-text">
                     <b>Name:</b> Gustavo Rodríguez Calzada <br>
-                    <b>Profile:</b> Web Developer | Ingeniero en Computación <br>
+                    <b>Profile:</b> Web Developer | Computer Engineer <br>
                     <b>Email:</b> gustavo.soader.cx@gmail.com <br>
                     <b>Phone:</b> (+52) 5611290464 <br>
                 </p>
@@ -15,3 +15,28 @@
     </div>
 </template>
 
+<style scoped>
+
+.card{
+    /*Margen en el top*/
+    margin-top: 5%;
+    /*Margen del lado izquierdo*/
+    margin-left: 5%;
+    /*Borde*/
+    border: 1px solid rgb(142, 209, 221);
+    /*Color de fondo*/
+    background-color: #a1d0db;
+    /*Color de texto*/
+    color: black;
+    /*Alineación del texto*/
+    text-align: left;
+    /*Tamaño de fuente*/
+    font-size: 18px;
+    /* Borde redondo */
+    border-radius: 10px;
+    /* Sombra en la carta */
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);    
+    
+}
+
+</style>
