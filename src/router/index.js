45c074c90efcea892b25fc1bac1c9eb0473cd3e6
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2022-03-23 21:05:40 -0600

    añadiendo rutas

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "quiensoy" */ '../views/QuienSoyView.vue')
+    component: () => import(/* webpackChunkName: "quiensoy" */ '../views/QuienSoy.vue')
   },
   {
     path: '/contacto',
