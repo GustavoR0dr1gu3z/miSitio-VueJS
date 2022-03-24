b1cb444566e6aa194501a3f19d43e62707f41080
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2022-03-23 21:53:20 -0600

    colocando imagen en quien soy

--- src/views/QuienSoy.vue
+++ src/views/QuienSoy.vue
@@ -1,5 +1,17 @@
 <template>
     <div id="nav">
-        Quien soy
+        <div class="card" style="width: 25rem;">
+            <img src="../../public/img/imgWeb/gus.jpg" class="card-img-top" alt="niñoGuapo">            
+            
+            <div class="card-body">
+                <p class="card-text">
+                    <b>Name:</b> Gustavo Rodríguez Calzada <br>
+                    <b>Profile:</b> Web Developer | Ingeniero en Computación <br>
+                    <b>Email:</b> gustavo.soader.cx@gmail.com <br>
+                    <b>Phone:</b> (+52) 5611290464 <br>
+                </p>
+            </div>
+</div>
     </div>
 </template>
+
