359183f8d1e55160167e6766c56cd665351cdae5
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2022-03-27 19:13:28 -0600

    editando estilos de la carta

--- src/views/ExperienciaLaboral.vue
+++ src/views/ExperienciaLaboral.vue
@@ -51,6 +51,7 @@ a:hover {
 	transition: color 0.3s ease-in-out;
 }
 
+/* Titulo de MI TRAYECTORIA LABORAL */
 #pageHeaderTitle {
 	margin: 2rem 0;
 	text-transform: uppercase;
@@ -58,11 +59,10 @@ a:hover {
 	font-size: 2.5rem;
 }
 
-/* Cards */
+/* Cartas */
 .postcard {
 	flex-wrap: wrap;
 	display: flex;
-
 	box-shadow: 0 4px 21px -12px rgba(0, 0, 0, 0.66);
 	border-radius: 10px;
 	margin: 0 0 2rem 0;
@@ -76,139 +76,12 @@ a:hover {
 		flex-wrap: inherit;
 	}
 }
+
 @media screen and (min-width: 1024px) {
 	.postcard__text {
 		padding: 2rem 3.5rem;
 	}
 }
 
-/* COLORS */
-.postcard .postcard__tagbox .green.play:hover {
-	background: #79dd09 ;
-	color: black;
-}
-.green .postcard__title:hover {
-	color: #79dd09 ;
-}
-.green .postcard__bar {
-	background-color: #79dd09 ;
-}
-.green::before {
-	background-image: linear-gradient(
-		-30deg,
-		rgba(121, 221, 9, 0.1) ,
-		transparent 50%
-	);
-}
-.green:nth-child(2n)::before {
-	background-image: linear-gradient(30deg, rgba(121, 221, 9, 0.1) , transparent 50%);
-}
-
-.postcard .postcard__tagbox .blue.play:hover {
-	background: #0076bd ;
-}
-.blue .postcard__title:hover {
-	color: #0076bd ;
-}
-.blue .postcard__bar {
-	background-color: #0076bd ;
-}
-.blue::before {
-	background-image: linear-gradient(-30deg, rgba(0, 118, 189, 0.1) , transparent 50%);
-}
-.blue:nth-child(2n)::before {
-	background-image: linear-gradient(30deg, rgba(0, 118, 189, 0.1) , transparent 50%);
-}
-
-.postcard .postcard__tagbox .red.play:hover {
-	background: #bd150b ;
-}
-.red .postcard__title:hover {
-	color: #bd150b ;
-}
-.red .postcard__bar {
-	background-color: #bd150b ;
-}
-.red::before {
-	background-image: linear-gradient(-30deg, rgba(189, 21, 11, 0.1) , transparent 50%);
-}
-.red:nth-child(2n)::before {
-	background-image: linear-gradient(30deg, rgba(189, 21, 11, 0.1) , transparent 50%);
-}
-
-.postcard .postcard__tagbox .yellow.play:hover {
-	background: #bdbb49 ;
-	color: black;
-}
-.yellow .postcard__title:hover {
-	color: #bdbb49 ;
-}
-.yellow .postcard__bar {
-	background-color: #bdbb49 ;
-}
-.yellow::before {
-	background-image: linear-gradient(
-		-30deg,
-		rgba(189, 187, 73, 0.1) ,
-		transparent 50%
-	);
-}
-.yellow:nth-child(2n)::before {
-	background-image: linear-gradient(
-		30deg,
-		rgba(189, 187, 73, 0.1) ,
-		transparent 50%
-	);
-}
-
-@media screen and (min-width: 769px) {
-	.green::before {
-		background-image: linear-gradient(
-			-80deg,
-			rgba(121, 221, 9, 0.1) ,
-			transparent 50%
-		);
-	}
-	.green:nth-child(2n)::before {
-		background-image: linear-gradient(
-			80deg,
-			rgba(121, 221, 9, 0.1) ,
-			transparent 50%
-		);
-	}
-
-	.blue::before {
-		background-image: linear-gradient(
-			-80deg,
-			rgba(0, 118, 189, 0.1) ,
-			transparent 50%
-		);
-	}
-	.blue:nth-child(2n)::before {
-		background-image: linear-gradient(80deg, rgba(0, 118, 189, 0.1) , transparent 50%);
-	}
-
-	.red::before {
-		background-image: linear-gradient(-80deg, rgba(189, 21, 11, 0.1) , transparent 50%);
-	}
-	.red:nth-child(2n)::before {
-		background-image: linear-gradient(80deg, rgba(189, 21, 11, 0.1) , transparent 50%);
-	}
-
-	.yellow::before {
-		background-image: linear-gradient(
-			-80deg,
-			rgba(189, 187, 73, 0.1) ,
-			transparent 50%
-		);
-	}
-	.yellow:nth-child(2n)::before {
-		background-image: linear-gradient(
-			80deg,
-			rgba(189, 187, 73, 0.1) ,
-			transparent 50%
-		);
-	}
-}
 
 </style>
