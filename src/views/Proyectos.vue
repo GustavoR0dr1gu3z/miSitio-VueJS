07844daa6aa6163db57166050d57f54f74c51091
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2022-03-28 22:53:39 -0600

    Añadiendo mis proyectos

--- src/views/Proyectos.vue
+++ src/views/Proyectos.vue
@@ -1,5 +1,96 @@
 <template>
     <div id="nav">
-        Proyectos
+        <section id="work" class="portfolio-mf sect-pt4 route">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="title-box text-center">
+                            <h3 class="title-a">
+                                Proyectos
+                            </h3>
+                            <p class="subtitle-a">
+                                A continuación colocaré los proyectos que he realizado, si dan clic en la imagen los direccionará a una plataforma llamada GitHub, ahí estaré subiendo todos mis proyectos.
+                                Una vez dado clic lo que harán es buscar el boton "Clone", seguidamente "Download ZIP" para que comience la descarga.
+                            </p>
+                            <div class="line-mf"></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">                    
+                    <div class="col-md-4">
+                        <div class="work-box">
+                            <a href="https://github.com/GustavoR0dr1gu3z/UNAMxHack">
+                                <div class="work-img">
+                                    <img src="img/difusion.png" alt="" class="img-fluid">
+                                </div>
+                                <div class="work-content">
+                                    <div class="row">
+                                        <div class="col-sm-8">
+                                            <h2 class="w-title">Página web realizada en UNAMxHacks</h2>
+                                            <div class="w-more">
+                                                <h6>
+                                                    Página web donde nosotros impartimos cultura de toda la república mexicana, anexandop dicho mapa y dividiendo los estados en botones, y cada uno de ellos tendrá información (Monumentos importantes, Datos interesantes) así como una imagen que deberá ser escaneada con una aplicación móvil que realizamos, y se verá en realidad aumentada. <br> Además contiene un audiolibro para la fácil lectura de algunas personas.
+                                                </h6>
+                                                <span class="w-ctegory">Web Design</span> / <span class="w-date">26 Oct. 2019</span>
+                                            </div>
+                                        </div>                 
+                                    </div>
+                                </div>
+                            </a>            
+                        </div>          
+                    </div>                      
+                </div>
+
+                <div class="col-md-4">
+                    <div class="work-box">
+                        <a href="https://github.com/GustavoR0dr1gu3z/Blender_Espada">
+                            <div class="work-img">
+                                <img src="img/espada.png" alt="" class="img-fluid">
+                            </div>
+
+                            <div class="work-content">
+                                <div class="row">
+                                    <div class="col-sm-8">
+                                        <h2 class="w-title">Espada 3D hecha en Blender</h2>
+                                        <div class="w-more">
+                                            <h6>
+                                                Réplica de una Espada Jedi, de la saga Star Wars lista para ser usada para diferentes proyectos como por ejemplo para Unity3D o diferentes plataformas que te permitan usar objetos 3D.
+                                            </h6>
+                                            <span class="w-ctegory">3D Animation</span> / <span class="w-date">22 Apr. 2020</span>
+                                        </div>
+                                    </div>                  
+                                </div>
+                            </div>
+                        </a>            
+                    </div>                    
+                </div>   
+                
+                
+                <div class="col-md-4">
+                    <div class="work-box">
+                        <a href="https://github.com/GustavoR0dr1gu3z/Detector_Personas_RNN">
+                        <div class="work-img">
+                            <img src="img/personas.jpg" alt="" class="img-fluid">
+                        </div>
+                            <div class="work-content">
+                                <div class="row">
+                                    <div class="col-sm-8">
+                                        <h2 class="w-title">Detector de personas con una neurona</h2>        
+                                        <div class="w-more">
+                                            <h6>
+                                                Proyecto de un detector de personas usando una neurona, donde se escoge una imagen y predice si es un humano o no.
+                                            </h6>
+                                            <span class="w-ctegory">Detector de personas</span> / <span class="w-date">01 Dic. 2020</span>
+                                        </div>
+                                    </div>                 
+                                </div>
+                            </div>
+                        </a>            
+                    </div>          
+                </div>   
+            </div>  
+        </section>
     </div>
+
 </template>
