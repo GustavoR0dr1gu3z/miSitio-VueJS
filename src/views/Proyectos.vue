d1b36f5c6288e1adb2a4d0d0a7796395ed2e6f85
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2022-03-28 23:26:04 -0600

    seccion de proyectos

--- src/views/Proyectos.vue
+++ src/views/Proyectos.vue
@@ -16,14 +16,14 @@
                         </div>
                     </div>
                 </div>
-
-                <div class="row">                    
+            <div class="tarjetas">              
                     <div class="col-md-4">
                         <div class="work-box">
                             <a href="https://github.com/GustavoR0dr1gu3z/UNAMxHack">
                                 <div class="work-img">
-                                    <img src="img/difusion.png" alt="" class="img-fluid">
+                                    <img src="../../public/img/imgWeb/difusion.png" alt="" class="img-fluid">
                                 </div>
+                            </a>
                                 <div class="work-content">
                                     <div class="row">
                                         <div class="col-sm-8">
@@ -36,19 +36,17 @@
                                             </div>
                                         </div>                 
                                     </div>
-                                </div>
-                            </a>            
-                        </div>          
-                    </div>                      
+                                </div>           
+                        </div>                            
                 </div>
 
                 <div class="col-md-4">
                     <div class="work-box">
                         <a href="https://github.com/GustavoR0dr1gu3z/Blender_Espada">
                             <div class="work-img">
-                                <img src="img/espada.png" alt="" class="img-fluid">
+                                <img src="../../public/img/imgWeb/espada.png" alt="" class="img-fluid">
                             </div>
-
+                        </a>
                             <div class="work-content">
                                 <div class="row">
                                     <div class="col-sm-8">
@@ -62,7 +60,6 @@
                                     </div>                  
                                 </div>
                             </div>
-                        </a>            
                     </div>                    
                 </div>   
                 
@@ -70,27 +67,147 @@
                 <div class="col-md-4">
                     <div class="work-box">
                         <a href="https://github.com/GustavoR0dr1gu3z/Detector_Personas_RNN">
-                        <div class="work-img">
-                            <img src="img/personas.jpg" alt="" class="img-fluid">
-                        </div>
-                            <div class="work-content">
-                                <div class="row">
-                                    <div class="col-sm-8">
-                                        <h2 class="w-title">Detector de personas con una neurona</h2>        
-                                        <div class="w-more">
-                                            <h6>
-                                                Proyecto de un detector de personas usando una neurona, donde se escoge una imagen y predice si es un humano o no.
-                                            </h6>
-                                            <span class="w-ctegory">Detector de personas</span> / <span class="w-date">01 Dic. 2020</span>
-                                        </div>
-                                    </div>                 
-                                </div>
+                            <div class="work-img">
+                                <img src="../../public/img/imgWeb/personas.jpg" alt="" class="img-fluid">
                             </div>
-                        </a>            
+                        </a>
+                                <div class="work-content">
+                                    <div class="row">
+                                        <div class="col-sm-8">
+                                            <h2 class="w-title">Detector de personas con una neurona</h2>        
+                                            <div class="w-more">
+                                                <h6>
+                                                    Proyecto de un detector de personas usando una neurona, donde se escoge una imagen y predice si es un humano o no.
+                                                </h6>
+                                                <span class="w-ctegory">Detector de personas</span> / <span class="w-date">01 Dic. 2020</span>
+                                            </div>
+                                        </div>                 
+                                    </div>
+                                </div>                        
                     </div>          
                 </div>   
-            </div>  
+            </div> 
+            </div> 
         </section>
     </div>
 
 </template>
+
+
+<style scoped>
+
+.box-shadow,
+.paralax-mf,
+.service-box,
+.work-box,
+.card-blog {
+  box-shadow: 0 13px 8px -10px rgba(0, 0, 0, 0.1);
+}
+
+.title-s {
+  font-weight: 600;
+  color: #1e1e1e;
+  font-size: 1.1rem;
+}
+
+/*--/ Title A /--*/
+
+.title-box {
+  margin-bottom: 4rem;
+}
+
+.title-a {
+  font-size: 3rem;
+  font-weight: bold;
+  text-transform: uppercase;
+}
+
+.subtitle-a {
+  color: #4e4e4e;
+}
+
+.line-mf {
+  width: 40px;
+  height: 5px;
+  background-color: #0078ff;
+  margin: 0 auto;
+}
+
+/*--/ Title Left /--*/
+
+.title-box-2 {
+  margin-bottom: 3rem;
+}
+
+.title-left {
+  font-size: 2rem;
+  position: relative;
+}
+
+.title-left:before {
+  content: '';
+  position: absolute;
+  height: 3px;
+  background-color: #0078ff;
+  width: 100px;
+  bottom: -12px;
+}
+
+.work-box {
+  margin-bottom: 3rem;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+  background-color: #fff;
+}
+
+.work-box:hover img {
+  -webkit-transform: scale(1.3);
+  transform: scale(1.3);
+}
+
+.work-img {
+  display: block;
+  overflow: hidden;
+}
+
+.work-img img {
+  transition: all 1s;
+}
+
+.work-content {
+  padding: 2rem 3% 1rem 4%;
+}
+
+.work-content .w-more {
+  color: #4e4e4e;
+  font-size: .8rem;
+}
+
+.work-content .w-more .w-ctegory {
+  color: #0078ff;
+}
+
+.work-content .w-like {
+  font-size: 2.5rem;
+  color: #0078ff;
+  float: right;
+}
+
+.work-content .w-like a {
+  color: #0078ff;
+}
+
+.work-content .w-like .num-like {
+  font-size: .7rem;
+}
+
+.w-title {
+  font-size: 1.2rem;
+}
+
+.tarjetas {    
+    display: flex;
+    justify-content: space-between;
+}
+
+</style>
